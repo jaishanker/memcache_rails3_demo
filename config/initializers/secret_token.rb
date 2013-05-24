f4df4655cd1b3714c61995f5ccc95ed86682dc8c
jaishanker <jaishanker123@gmail.com>
2013-05-24 15:18:14 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MemcacheDemo::Application.config.secret_token = 'f97638babd496affe195e033903a31a09a5bb4c89ccacfb4191474bcd43656c20c7b910c740b870a5fcae4ce708a195fcd7df2143fba16f4beedb8e4b04ad4b0'
